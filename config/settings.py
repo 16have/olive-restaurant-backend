f41461a857c35a13dcda15cec3022097a9883aac
kelvin <joer88544@gmail.com>
2026-07-27 09:37:15 +0300

    database url

--- config/settings.py
+++ config/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 
@@ -135,9 +135,3 @@ CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https://olive-restaurant-frontend.*\.vercel\.app$",
     r"^https://olive-restaurant-frontend.onrender.com"
 ]
-DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-    )
-}
