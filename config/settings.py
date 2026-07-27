b78d52b23ae6529b01f40f03f851ec4a0a0083e7
kelvin <joer88544@gmail.com>
2026-07-27 09:32:38 +0300

    Configure PostgreSQL for production using DATABASE_URL

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import dj_database_url
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -134,3 +135,9 @@ CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https://olive-restaurant-frontend.*\.vercel\.app$",
     r"^https://olive-restaurant-frontend.onrender.com"
 ]
+DATABASES = {
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
+}
